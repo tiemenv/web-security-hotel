e626da7119fde95bed3b46aa0eb7e7903e59a268
= <=>
2019-02-27 15:17:47 +0100

    hello-world attempt

--- server-side/public/index.php
+++ server-side/public/index.php
@@ -1,16 +1,10 @@
 <?php
+echo "hello from serverside";
+exit;
 
 require('../vendor/autoload.php');
 require('../lib.php');
 require('../routes.php');
 require('../bootstrap.php');
 
-echo "hello from serverside";
-exit;
-
 render(run($routes));
-
-
-
-
-
